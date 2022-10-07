be57e0365eed87cf4d144b7657c54dbeddad11d4
Umesh U <umeshtechdeveloper@gmail.com>
2022-10-07 12:33:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Download, Features, SectionWrapper } from "./components";
+import assets from "./assets";
+import styles from "./styles/Global";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+     <SectionWrapper
+       title="Your own store of Nifty NFTS. Start selling and Growing."
+       description="Buy, store, collect NFTs, exchange & earn crypto. Join 25+ million people using ProNef Marketplace."
+       showBtn
+       mockUpImg={assets.homeHero}
+       banner="banner"
+     />
+     <SectionWrapper
+       title="Smart User Interface Marketplace"
+       description="Experience a buttery UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI design"
+       mockUpImg={assets.homeCards}
+       reverse
+     />
+     <Features />
+     <SectionWrapper
+       title="Deployment"
+       description="ProNef is built using Expo which runs natively on all devices. You will enjoy the smooth UI experience"
+       showBtn
+       mockUpImg={assets.feature}
+       banner="banner"
+     />
+     <SectionWrapper
+       title="Creative way to showcase the store"
+       description="The app contain two screens. The first screen lists all NFTs while second one shows the details of a specific NFT."
+       mockUpImg={assets.mockup}
+       banner="banner02"
+     />
+     <Download />
+     <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+       <p className={`${styles.pText} ${styles.whiteText}`}>Made with love by {""}
+        <span className="bold">Umesh U</span>
+       </p>
+     </div>
+    </>
+  )
 }
 
 export default App;
